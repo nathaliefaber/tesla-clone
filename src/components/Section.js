75be06d0f42aebf5f0d55513a8649a1f7af1c4ef
Nathalie Faber <nathalie_faber@hotmail.com>
2022-01-31 17:32:56 +0100

    created project and headstart on homepage

---
+++ src/components/Section.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+function Section() {
+  return <Wrap></Wrap>;
+}
+
+export default Section;
+
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-image: url("/images/model-s.jpg");
+`;
